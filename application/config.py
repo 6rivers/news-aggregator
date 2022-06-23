aae7d2ceca52287f3d573b30b70100db6597fee1
6rivers <sranganath87@gmail.com>
2022-06-23 21:13:55 +0530

    'env'

--- application/config.py
+++ application/config.py
@@ -7,5 +7,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'tough-to-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
